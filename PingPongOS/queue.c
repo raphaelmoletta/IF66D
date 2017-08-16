e2ce20b1bf2a7463e1ffb71dce85a0951a87abc0
a1193260 <a1193260@q212-4c>
2017-08-16 20:10:52 -0300

    queue.c

---
+++ PingPongOS/queue.c
@@ -0,0 +1,44 @@
+#include"queue.h"
+#include<stdio.h>
+
+void queue_append (queue_t **queue, queue_t *elem)
+{
+    
+}
+
+//------------------------------------------------------------------------------
+// Remove o elemento indicado da fila, sem o destruir.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - a fila nao deve estar vazia
+// - o elemento deve existir
+// - o elemento deve pertencer a fila indicada
+// Retorno: apontador para o elemento removido, ou NULL se erro
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem)
+{
+    
+}
+
+//------------------------------------------------------------------------------
+// Conta o numero de elementos na fila
+// Retorno: numero de elementos na fila
+
+int queue_size (queue_t *queue)
+{
+    
+}
+
+//------------------------------------------------------------------------------
+// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
+// elemento é feita por uma função externa, definida pelo programa que
+// usa a biblioteca.
+//
+// Essa função deve ter o seguinte protótipo:
+//
+// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) )
+{
+    
+}
